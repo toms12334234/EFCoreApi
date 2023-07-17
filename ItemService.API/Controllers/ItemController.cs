5ae914c4356947e4ad69afc4655cdb616d630632
Tom Schenkels <tomsch1kels@gmail.com>
2023-07-17 16:01:12 +0200

    feat: First working implementation

--- ItemService.API/Controllers/ItemController.cs
+++ ItemService.API/Controllers/ItemController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ItemService.Data;
+using ItemService.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ItemService.API.Controllers
 {
@@ -6,22 +9,35 @@ namespace ItemService.API.Controllers
     [Route("[controller]/[action]")]
     public class ItemController : ControllerBase
     {
+        private readonly AppDbContext _appDbContext;
+
+        public ItemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        
         [HttpGet]
         public async Task<object> GetItems()
         {
-            return await Task.FromResult("Hello");
+            return _appDbContext.Items;
         }
         
         [HttpGet]
         public async Task<object> GetItem(int id)
         {
-            return await Task.FromResult("Hello");
+            Item item = _appDbContext.Items.FirstOrDefault(x=>x.Id == id);
+            return item != null 
+                ? new OkObjectResult(item)
+                : new NotFoundResult();
         }
 
         [HttpPost]
         public async Task<object> AddItem(string name)
         {
-            return await Task.FromResult("Hello");
+            Item item = new Item(){Name=name};
+            await _appDbContext.Items.AddAsync(item);
+            await _appDbContext.SaveChangesAsync();
+            return Created("this", item);
         }
     }
 }
